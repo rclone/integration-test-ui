4d196e4672a10f2f167d02efb6e4cade3f26a986
dougal <dougal.craigwood@gmail.com>
2025-10-01 17:43:54 +0100

    Highlight filtered items

--- src/FailedTests.jsx
+++ src/FailedTests.jsx
@@ -10,6 +10,8 @@ import { useState } from 'react'
 function FailedTests() {
     const [filter, setFilter] = useState();
 
+    const styling = (test) => ({ cursor: "pointer", backgroundColor: test === filter ? "red" : "transparent" })
+
     return (
         <>
             <h3>Failed Tests: {data.Failed.length}</h3>
@@ -31,13 +33,13 @@ function FailedTests() {
                         || (item.FailedTests ?? []).includes(filter)
                         : true).map((item, i) => (
                             <tr key={i}>
-                                <td style={{ cursor: "pointer" }} onClick={() => setFilter(item.Backend)}>{item.Backend}</td>
-                                <td style={{ cursor: "pointer" }} onClick={() => setFilter(item.Remote)}>{item.Remote}</td>
-                                <td style={{ cursor: "pointer" }} onClick={() => setFilter(item.Path)}>{item.Path}</td>
-                                <td style={{ cursor: "pointer" }} onClick={() => setFilter(item.FastList)}>{String(item.FastList)}</td>
+                                <td style={styling(item.Backend)} onClick={() => setFilter(item.Backend)}>{item.Backend}</td>
+                                <td style={styling(item.Remote)} onClick={() => setFilter(item.Remote)}>{item.Remote}</td>
+                                <td style={styling(item.Path)} onClick={() => setFilter(item.Path)}>{item.Path}</td>
+                                <td style={styling(item.FastList)} onClick={() => setFilter(item.FastList)}>{String(item.FastList)}</td>
                                 <td>
                                     {(item.FailedTests ?? []).map((test, idx) => (
-                                        <div key={idx} style={{ cursor: "pointer" }} onClick={() => setFilter(test)}>{test}</div>
+                                        <div key={idx} style={styling(test)} onClick={() => setFilter(test)}>{test}</div>
                                     ))}
                                 </td>
                                 <td>{item.TrialNames}</td>
