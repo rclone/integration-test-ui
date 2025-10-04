25e4383e516d6cb0c75cf5ebb21f26c57ec565f6
dougal <dougal.craigwood@gmail.com>
2025-10-04 15:38:39 +0100

    icon and URL parameters

--- src/FailedTests.jsx
+++ src/FailedTests.jsx
@@ -1,10 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect, useLayoutEffect } from 'react'
 import { useData } from './DataContext'
+
+const parseFilter = v => {
+    if (v === null) return null
+    if (v === "true") return true
+    if (v === "false") return false
+    return v;
+}
 
 export default function FailedTests() {
     const [filter, setFilter] = useState(null);
+    const { data, selected, setSelected } = useData();
+    const [ready, setReady] = useState(false)
     const styling = (test) => ({ cursor: "pointer", backgroundColor: test === filter ? "red" : "transparent" })
-    const { selected } = useData();
+
+    useLayoutEffect(() => {
+        // set filter and selected from URL
+        if (!data) return
+
+        const params = new URLSearchParams(window.location.search)
+        const f = params.get("filter")
+        const d = params.get("date")
+
+        setFilter(parseFilter(f))
+        if (d !== null) {
+            const match = data.find(t => String(t.DateTime) === d)
+            if (match) setSelected(match)
+        }
+
+        setReady(true)
+    }, [data])
+
+    useEffect(() => {
+        // changes url based on filter and selected
+        if (!ready) return
+
+        const params = new URLSearchParams(window.location.search)
+        if (filter !== null && filter !== undefined) {
+            params.set("filter", String(filter))
+        } else params.delete("filter")
+        if (selected?.DateTime) params.set("date", String(selected.DateTime)); else params.delete("date")
+
+        const qs = params.toString()
+        const newURL = `${window.location.pathname}${qs ? `?${qs}` : ""}`
+        const oldURL = `${window.location.pathname}${window.location.search}`
+        if (newURL !== oldURL) window.history.replaceState(null, "", newURL)
+    }, [filter, selected])
+
     selected.Failed.map((t) => (
         t.FailedTests ??= ["DID NOT COMPLETE"]
     ))
